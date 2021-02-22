390e11d17d8f8961075fc3315c89d4c6bdb7d901
Angelika Blackburn <esonyei@gmail.com>
2021-02-21 19:50:29 -0500

    projects GET

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,9 +1,21 @@
 // Write your "projects" router here!
-
 const express = require("express");
-
+const projects = require("./projects-model");
 const router = express.Router();
 
-router.get("/", (req, res) => {});
+router.get("/", async (req, res, next) => {
+	try {
+		const fetched = await projects.get();
+		res.status(200).json(fetched);
+	} catch (err) {
+		next(err);
+	}
+});
+
+// get,
+// insert,
+// update,
+// remove,
+// getProjectActions,
 
 module.exports = router;
